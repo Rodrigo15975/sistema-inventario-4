3af03dc3b6a441511ed9ac71df6678de7f1aaac7
rodrigo15975 <javierluis15975@hotmail.com>
2024-09-23 00:27:14 -0500

    config-react-query

---
+++ src/config/reactQueryProvider.js
@@ -0,0 +1,13 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+export { queryClient, QueryClientProvider, ReactQueryDevtools }
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { PrimeReactProvider } from 'primereact/api'
+import 'primereact/resources/themes/lara-light-cyan/theme.css'
+import { BrowserRouter } from 'react-router-dom'
+import {
+  queryClient,
+  QueryClientProvider,
+  ReactQueryDevtools,
+} from './config/reactQueryProvider.js'
 
-import { BrowserRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PrimeReactProvider>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools buttonPosition="bottom-right" />
+        </QueryClientProvider>
       </BrowserRouter>
     </PrimeReactProvider>
   </StrictMode>
-);
+)
